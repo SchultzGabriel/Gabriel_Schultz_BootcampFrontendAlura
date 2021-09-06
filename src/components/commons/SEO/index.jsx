69ace273d84a4cf9547fe11bd94f911e8eedb920
Gabriel Bridi Schultz <gabriel.bridi2008@hotmail.com>
2021-09-05 23:06:13 -0300

    refactor(refactoring seo): refactoring seo according with metatags.io best settings

--- src/components/commons/SEO/index.jsx
+++ src/components/commons/SEO/index.jsx
@@ -10,9 +10,6 @@ export default function SEO({ headTitle }) {
     : baseTitle;
 
   const description = 'Apresento meu portfólio, onde você pode encontrar informações sobre meu trabalho, skills e localização.';
-  // criadora da imagem abaixo: https://github.com/carolandrade1
-  const image = 'https://i.imgur.com/c2kcPN7.png';
-  const urlBase = 'urldomeuprojeto.com.br';
 
   return (
     <Head>
@@ -20,19 +17,17 @@ export default function SEO({ headTitle }) {
       <meta name="title" content={title} />
       <meta name="description" content={description} />
 
-      {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={urlBase} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:url" content="https://gabriel-schultz-portfolio-online.vercel.app/" />
+      <meta property="og:title" content="Home | Gabriel Schultz - Portfólio" />
+      <meta property="og:description" content="Apresento meu portfólio, onde você pode encontrar informações sobre meu trabalho, skills e localização." />
+      <meta property="og:image" content="https://i.imgur.com/kIZjmVf.png" />
 
-      {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={urlBase} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={image} />
+      <meta property="twitter:url" content="https://gabriel-schultz-portfolio-online.vercel.app/" />
+      <meta property="twitter:title" content="Home | Gabriel Schultz - Portfólio" />
+      <meta property="twitter:description" content="Apresento meu portfólio, onde você pode encontrar informações sobre meu trabalho, skills e localização." />
+      <meta property="twitter:image" content="https://i.imgur.com/kIZjmVf.png" />
     </Head>
   );
 }
